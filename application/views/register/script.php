1bb0b0ed1678b7302a5d054e9f5ea96b41a61df0
unknown <natlove.natlove202@gmail.com>
2019-04-08 22:35:26 +0700

    fix a littal bit

--- application/views/register/script.php
+++ application/views/register/script.php
@@ -1,7 +1,5 @@
 <script>
-    // alert("hello");
-    password1 = document.getElementById("password").value;
-    password2 = document.getElementById("repassword").value;
+
     
 $("#insert").validate({
         rules: {
@@ -41,18 +39,10 @@ $("#insert").validate({
     });
     $("#insert").submit(function(){
         login();
-        // checkpassword(password1,password2);
+        
         
     })
-    // function checkPassword(password1,password2) { 
-        
-    //            if (password1 != password2) { 
-    //                 alert ("\nPassword did not match: Please try again...") 
-    //                 return false; 
-    //             }else{
-    //                 login();
-    //             }
-    //         } 
+    
     function login(){
         event.preventDefault();
         var isValid = $("#insert").valid();
@@ -65,16 +55,22 @@ $("#insert").validate({
             var name = {
                 "name": $("#name").val()
             }
-            if(data.repassword == data2.password){
-                if(isValid){
-                    alert("Register Has Complete Wellcome   "+name.name)
-                window.location.replace( href="<?=base_url("welcome") ?>");
-  
-                }
-            
-            }else{
-                alert("Password not match!!")
+            var email = {
+                "email": $("#email").val()
             }
+           
+            if(isValid){
+                if(data.repassword !="" && data2.password != "" && name.name !="" &&email.email !=""){
+                    if(data.repassword == data2.password){
+                        alert("Register Has Complete Wellcome   "+name.name)
+                        window.location.replace( href="<?=base_url("welcome") ?>");
+                    }else{
+                        alert("Password not match!!")
+                    }
+                }
+                    
+            }
+            
         
         
     }
