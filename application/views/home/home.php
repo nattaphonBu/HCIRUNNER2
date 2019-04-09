81310a0cfef63b55ed45a75255ab2a6bfc1dc0ae
unknown <natlove.natlove202@gmail.com>
2019-04-09 20:43:47 +0700

    config url

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -26,14 +26,14 @@
 	  
     <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand" href="home.html"><span>RUN MY WAY.</span></a>
+	      <a class="navbar-brand"  href="<?=base_url("Welcome") ?>"><span>RUN MY WAY.</span></a>
 	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> Menu
 	      </button>
       
 	      <div class="collapse navbar-collapse" id="ftco-nav">
 	        <ul class="navbar-nav nav ml-auto">
-	          <li class="nav-item"><a href="home.html" class="nav-link" data-nav-section="home"><span>Home</span></a></li>
+	          <li class="nav-item"><a href="<?=base_url("Welcome") ?>" class="nav-link" data-nav-section="home"><span>Home</span></a></li>
 	        </ul>
         </div>
         <ul class="navbar-nav nav ml-auto">
