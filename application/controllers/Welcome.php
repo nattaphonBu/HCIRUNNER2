d3b47f7b35292c4513e4ac5af1945be2b21129fe
guyvalkyrie <Guyfreedom02@gmail.com>
2019-05-17 22:29:07 +0700

    add welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -164,5 +164,14 @@ class Welcome extends CI_Controller {
 		$this->load->view('template/foot');
 		$this->load->view('template/footer');
 	}
+	public function paymentcheck()
+	{
+		// $this->load->view('home/home');
+		$this->load->view('template/head');
+		$this->load->view('template/header');
+		$this->load->view('payment/payment');
+		$this->load->view('template/foot');
+		$this->load->view('template/footer');
+	}
 	
 }
---
+++ application/views/payment/payment.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<title>FOR BURININININ</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta charset="utf8">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"> -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500,700 ,900" rel="stylesheet">
+<style>
+    body{
+        font-family: Rubik;
+    }
+    .cen{
+        text-align: center;
+        
+    }
+</style>
+<body>
+    
+
+    <form>
+    <div class="container" id="bgbody">
+    <p><h1 class="cen">การจัดการงานวิ่ง</h1></p>
+        <div class="row">
+            <div class="col-sm-12 cen" > 
+            
+            </div>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
