2b162bc5c756aab2bf487d8cdb28042f674112ea
unknown <natlove.natlove202@gmail.com>
2019-05-23 09:15:13 +0700

    edit

--- application/views/template/header_admin.php
+++ application/views/template/header_admin.php
@@ -1,7 +1,7 @@
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
-	      <a class="navbar-brand"  href="<?=base_url("Welcome/adminHome") ?>"><span>RUN MY WAY.</span></a>
+	      <a class="navbar-brand"  href="<?=base_url("Welcome/adminHomes") ?>"><span>RUN MY WAY.</span></a>
 	      <button class="navbar-toggler js-fh5co-nav-toggle fh5co-nav-toggle" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="oi oi-menu"></span> 
 	      </button>
